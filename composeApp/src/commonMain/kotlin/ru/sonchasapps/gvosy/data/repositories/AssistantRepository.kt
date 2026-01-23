4786f01cf0d66b54c28a1c63bdaddfba2b68ff1f
Sofia Tkach <104835342+sofytk@users.noreply.github.com>
2026-01-23 17:31:17 +0300

    Delete composeApp directory

--- composeApp/src/commonMain/kotlin/ru/sonchasapps/gvosy/data/repositories/AssistantRepository.kt
+++
@@ -1,9 +0,0 @@
-package ru.sonchasapps.gvosy.data.repositories
-
-import ru.sonchasapps.gvosy.data.models.AssistantEntity
-import ru.sonchasapps.gvosy.data.models.AssistantRequest
-
-interface AssistantRepository {
-    suspend fun addAssistant(assistantRequest: AssistantRequest) : Result<AssistantEntity>
-    suspend fun getAssistant(id: Long) : AssistantEntity?
-}
--- composeApp/src/iosMain/kotlin/ru/sonchasapps/gvosy/MainViewController.kt
+++
@@ -1,6 +0,0 @@
-package ru.sonchasapps.gvosy
-
-//import androidx.compose.ui.window.ComposeUIViewController
-//import ru.sonchasapps.gvosy.activities.App
-//
-//fun MainViewController() = ComposeUIViewController { App() }
